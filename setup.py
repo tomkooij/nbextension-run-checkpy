98aad75e98a38d7dc1e0774b386d55f49caa4289
Tom Kooij <tomkooij@tomkooij.nl>
2018-01-06 22:14:37 +0100

    Reformat setup.py

--- setup.py
+++ setup.py
@@ -10,19 +10,18 @@ except:
 VERSION = '0.1'
 
 setup(
-    name                = 'nbextension-run-checkpy',
-    version             = VERSION,
-    install_requires    = ['jupyter-pip'],
-    description         = """An extension of the Jupyter Notebook that runs checkPy.""",
-    cmdclass            = cmdclass('run-checkpy', 'run-checkpy/run-checkpy.js'),
-    author              = 'Tom Kooij',
-    author_email        = 'tom.kooij@gmail.com',
-	license             = 'MIT',
-	keywords           =  'new unexperienced programmers automatic testing minor programming',
-    classifiers         = ['Intended Audience :: Education',
-		                   'Topic :: Education :: Testing',
-		                   'License :: OSI Approved :: MIT License',
-                           'Programming Language :: Python'],
-    url                 = 'https://github.com/tomkooij/nbextension-run-checkpy',
-    download_url        = 'https://github.com/tomkooij/nbextension-run-checkpy/archive/%s.zip' % VERSION,
+    name='nbextension-run-checkpy',
+    version=VERSION,
+    description='An extension of the Jupyter Notebook that runs checkPy.',
+    author='Tom Kooij',
+    author_email='tom.kooij@gmail.com',
+	license='MIT',
+	classifiers=[
+        'Intended Audience :: Education',
+	    'Topic :: Education :: Testing',
+	    'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python'],
+    url='https://github.com/tomkooij/nbextension-run-checkpy',
+    cmdclass= cmdclass('run-checkpy', 'run-checkpy/run-checkpy.js'),
+    install_requires=['jupyter-pip'],
 )
